6845caf7cce38120b215978ac31eaebaece21ac8
George Tantiras <raratiru@users.noreply.github.com>
2022-03-20 22:52:30 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,6 +6,6 @@ from split_settings.tools import optional, include
 
 include(
     "central/01_default_settings.py",
-    "central/02_appetizer_settings.py",
+    "central/02_base_settings.py",
     optional("habitat/10_rehearsals.py"),
 )
