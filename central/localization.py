d67e0b9e9921d6a9cdabb3ad171de7b74847ede8
George Tantiras <raratiru@users.noreply.github.com>
2022-04-22 23:43:36 +0300

    Update localization.py

--- central/localization.py
+++ central/localization.py
@@ -13,7 +13,6 @@ LANGUAGE_CODE = "en"
 LANGUAGE_COOKIE_NAME = f"{config('POET_PROJECT')}_language"
 TIME_ZONE = "Europe/Athens"
 USE_I18N = True
-USE_L10N = True
 USE_THOUSAND_SEPARATOR = True
 USE_TZ = True
 LOCALE_PATHS = [
