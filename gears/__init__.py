9a4a006847537e16b7a602ff2ccacea006a31ebf
George Tantiras <raratiru@users.noreply.github.com>
2022-10-01 14:02:38 +0300

    Update __init__.py

--- gears/__init__.py
+++ gears/__init__.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Component Settings"""
+"""
+Component Settings
+
+Insert the file names as strings. For example:
+settings_filenames = ("foo.py", "bar.py")
+"""
+
+settings_filenames = ()
+gears_settings = ("gears/" + name for name in settings_filenames)
