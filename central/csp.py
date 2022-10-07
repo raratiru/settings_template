5efc7ecb725ba6db3fdfe76b7a3c0beaba7bc416
George Tantiras <raratiru@users.noreply.github.com>
2022-10-07 15:47:28 +0300

    Update csp.py

--- central/csp.py
+++ central/csp.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-CSP_SCRIPT_SRC = ("'self'",)
-CSP_IMG_SRC = ("'self'",)
-CSP_FONT_SRC = ("'self'",)
-CSP_STYLE_SRC = ("'self'",)
+CSP_CONNECT_SRC = ("'self'",)
 CSP_DEFAULT_SRC = ("'none'",)
+CSP_FONT_SRC = ("'self'",)
+CSP_IMG_SRC = ("'self'",)
+CSP_SCRIPT_SRC = ("'self'",)
+CSP_STYLE_SRC = ("'self'",)
