0fdf9b5a8d937c7e3b1215177f2480e717721e4d
raratiru <tantiras@yandex.com>
2025-04-11 17:29:39 +0300

    Corrected bug in development settings

--- habitat/development.py
+++ habitat/development.py
@@ -7,7 +7,7 @@ from main.settings.central.midcate import MIDDLEWARE
 
 DEBUG = True
 
-INSTALLED_APPS = INSTALLED_APPS + ["debug_toolbar"]
+INSTALLED_APPS = INSTALLED_APPS + ("debug_toolbar",)
 INTERNAL_IPS = ["127.0.0.1", "localhost"]  # Debug Toolbar
 MIDDLEWARE = MIDDLEWARE + [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
